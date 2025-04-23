8eeff16425b28fafeaa3da4e779a04266e5440a2
dhinakar1210 <dhinakargiri@gmail.com>
2025-04-22 19:01:38 -0700

    Updates

---
+++ precache-manifest.8ddfa7aa9492af5890118e6024bc72f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2557958b7ce612c3c1bdd432450a244a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbfcf8398b5c30e1690d",
+    "url": "/static/css/main.022eeef0.chunk.css"
+  },
+  {
+    "revision": "875023c694145a2e6611",
+    "url": "/static/js/2.36df0eb0.chunk.js"
+  },
+  {
+    "revision": "fbfcf8398b5c30e1690d",
+    "url": "/static/js/main.dfc6ec39.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
